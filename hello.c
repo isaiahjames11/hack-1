c35a2fd4c02f7bec9e48180f75efc7f11468df55
isaiahjames11 <42557619+isaiahjames11@users.noreply.github.com>
2018-08-23 09:59:26 -0500

    first commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Isaiah James and Chad Alioth
+ * Date: 2016/11/02
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+Isaiah James and Chad Alioth
+int main(int argc, char **argv) {
+
+  printf("Isaiah James Computer Science Major\n");
+
+  return 0;
+}
