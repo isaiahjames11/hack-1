eb21be3c9fe14321a8d679362acf6cb5c6f603c4
isaiahjames11 <42557619+isaiahjames11@users.noreply.github.com>
2018-08-23 10:16:54 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
 /**
  * Author: Isaiah James and Chad Alioth
  * Date: 2016/11/02
- *
+ *0
  * A simple hello world program in C
  *
  */
@@ -10,6 +10,6 @@
 int main(int argc, char **argv) {
 
   printf("Isaiah James Computer Science Major\n");
-
+  printf("Asmita Jayswal Computer Science Major\n");
   return 0;
 }
